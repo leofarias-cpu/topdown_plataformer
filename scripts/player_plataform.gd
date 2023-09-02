57293e31bbf378c536d44597e777cb94ade13238
leofarias-cpu <leonardo.almeida.farias@ccc.ufcg.edu.br>
2023-09-02 01:24:03 -0300

    Atualiza movimento em plataforma

--- scripts/player_plataform.gd
+++ scripts/player_plataform.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED: float
+@export var JUMP_VELOCITY: float
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,7 +14,8 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump"):
+		#and is_on_floor()
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
